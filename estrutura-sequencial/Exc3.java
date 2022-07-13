348bbe2dedc80bb9401e11e02f7b0978c8d4f41f
pedro <pedrohrds05@hotmail.com>
2022-07-13 09:43:56 -0300

    feat: exercise3

---
+++ estrutura-sequencial/Exc3.java
@@ -0,0 +1,23 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exc3 {
+  public static void main(String[] args) {
+    Locale.setDefault(Locale.US);
+
+    Scanner sc = new Scanner(System.in);
+
+    int A, B, C, D, result;
+
+    A = sc.nextInt();
+    B = sc.nextInt();
+    C = sc.nextInt();
+    D = sc.nextInt();
+
+    result = ((A * B) - (C * D));
+
+    System.out.printf("DIFERENCA = %d\n", result);
+
+    sc.close();
+  }   
+}
