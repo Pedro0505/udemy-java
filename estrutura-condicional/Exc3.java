5a2f62659cdb42abffdb8d5f0069bb7567c39f13
pedro <pedrohrds05@hotmail.com>
2022-07-13 15:01:22 -0300

    feat(estrutura-condicional): exercise 3

---
+++ estrutura-condicional/Exc3.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Exc3 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int A, B;
+
+    System.out.println("Digite um número:");
+
+    A = sc.nextInt();
+
+    System.out.println("Digite outro número:");
+
+    B = sc.nextInt();
+
+    if (A % B == 0 || B % A == 0) {
+      System.out.println("Sao Multiplos");
+    } else {
+      System.out.println("Nao sao Multiplos");
+    }
+
+    sc.close();
+  }   
+}
