a1583baacf691e51c46e06bd192f182f55a72e95
pedro <pedrohrds05@hotmail.com>
2022-07-13 09:32:03 -0300

    feat: exercise1

---
+++ estrutura-sequencial/Exc1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Exc1 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int x, y;
+
+    x = sc.nextInt();
+    y = sc.nextInt();
+
+    int sum =  x + y;
+
+    System.out.printf("SOMA = %d\n", sum);
+
+    sc.close();
+  }   
+}
