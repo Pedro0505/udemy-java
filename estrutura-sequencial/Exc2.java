84ecd2fdba09a45ac1d9381223d66d0119da0a94
pedro <pedrohrds05@hotmail.com>
2022-07-13 09:32:10 -0300

    feat: exercise2

---
+++ estrutura-sequencial/Exc2.java
@@ -0,0 +1,22 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exc2 {
+  public static void main(String[] args) {
+    Locale.setDefault(Locale.US);
+
+    Scanner sc = new Scanner(System.in);
+
+    double radius, result, pi;
+
+    radius = sc.nextDouble();
+
+    pi = 3.14159;
+
+    result = pi * Math.pow(radius, 2);
+
+    System.out.printf("A = %.4f\n", result);
+
+    sc.close();
+  }   
+}
