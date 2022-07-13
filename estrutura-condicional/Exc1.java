59ade4a8b2173f0017754a586eafd673360d37fa
pedro <pedrohrds05@hotmail.com>
2022-07-13 15:01:06 -0300

    feat(estrutura-condicional): exercise 1

---
+++ estrutura-condicional/Exc1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Exc1 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int num;
+
+    System.out.println("Digite um número:");
+
+    num = sc.nextInt();
+
+    if (num < 0) {
+      System.out.println("Negativo");
+    } else {
+      System.out.println("Não Negativo");
+    }
+
+    sc.close();
+  }   
+}
