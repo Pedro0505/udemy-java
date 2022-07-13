ef60ffeb547418380345aabccaec037add0d96a3
pedro <pedrohrds05@hotmail.com>
2022-07-13 20:37:38 -0300

    fix(estrutura-condicional): exercise 5

--- estrutura-condicional/Exc6.java
+++ estrutura-condicional/Exc6.java
@@ -9,16 +9,16 @@ public class Exc6 {
         System.out.println("Digite um valor:");
         value = sc.nextDouble();
         
-        if (value > 0 && value < 25) {
+        if (value >= 0 && value <= 25) {
             System.out.println("Intervalo ([0,25]");
         }
-        else if (value > 25 && value < 50) {
+        else if (value >= 25 && value <= 50) {
             System.out.println("Intervalo (25,50]");
         } 
-        else if (value > 50 && value < 70) {
+        else if (value >= 50 && value <= 70) {
             System.out.println("Intervalo (50,75]");
         }
-        else if (value > 75 && value < 100) {
+        else if (value >= 75 && value <= 100) {
             System.out.println("Intervalo  (75,100]");
         }
         else {
