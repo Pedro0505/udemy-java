db9f0f0bffadfd0309e4d294c9443faf1396ab85
pedro <pedrohrds05@hotmail.com>
2022-07-13 12:46:15 -0300

    feat: exercise4

---
+++ estrutura-sequencial/Exc4.java
@@ -0,0 +1,24 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exc4 {
+  public static void main(String[] args) {
+    Locale.setDefault(Locale.US);
+
+    Scanner sc = new Scanner(System.in);
+
+    int number, hours;
+    double recive, salary;
+
+    number = sc.nextInt();
+    hours = sc.nextInt();
+    recive = sc.nextDouble();
+
+    salary = hours * recive;
+
+    System.out.printf("NUMBER = %d\n", number);
+    System.out.printf("SALARY = U$ %.2f\n", salary);
+
+    sc.close();
+  }   
+}
