e44e2c7f1be5e75a46f6a809a193984930f3109e
pedro <pedrohrds05@hotmail.com>
2022-07-13 15:01:14 -0300

    feat(estrutura-condicional): exercise 2

---
+++ estrutura-condicional/Exc2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Exc2 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int num;
+
+    System.out.println("Digite um número:");
+
+    num = sc.nextInt();
+
+    if (num % 2 == 0) {
+      System.out.println("Par");
+    } else {
+      System.out.println("Ímpar");
+    }
+
+    sc.close();
+  }   
+}
