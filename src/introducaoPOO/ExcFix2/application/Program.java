b77f982fdf96bbe437acbcbca6b4186caf6bb4a5
pedro <pedrohrds05@hotmail.com>
2022-07-15 21:11:45 -0300

    feat(introducaoPoo): add fixation exercise 2

---
+++ src/introducaoPOO/ExcFix2/application/Program.java
@@ -0,0 +1,30 @@
+package introducaoPOO.ExcFix2.application;
+
+import java.util.Scanner;
+
+import introducaoPOO.ExcFix2.entites.Employee;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Employee employee = new Employee();
+
+        System.out.print("Name: ");
+        employee.name = sc.nextLine();
+        System.out.print("Gross Salary: ");
+        employee.grossSalary = sc.nextDouble();
+        System.out.print("Tax: ");
+        employee.tax = sc.nextDouble();
+
+        System.out.printf("Employee: %s, $ %.2f\n", employee.name, employee.netSalary());
+
+        System.out.print("Which percentage to increase salary? ");
+        double percentage = sc.nextDouble();
+        employee.increaseSalary(percentage);
+
+        System.out.printf("Updated data: %s, $ %.2f\n", employee.name, employee.netSalary());
+
+        sc.close();
+    }
+}
